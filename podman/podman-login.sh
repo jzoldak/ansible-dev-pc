1d0a78f5668be292fc5f1dd43a68b9d497dcd4ec
Jesse Zoldak <jesse.zoldak@clearballot.com>
2021-10-14 14:06:18 -0400

    Update podman

---
+++ podman/podman-login.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+podman-acr-login() {
+    local -r username='jesse.zoldak@clearballot.com'
+
+
+    acr_password() {
+        local -r registry="${1}"
+
+        set +x
+        az acr login \
+            --name "${registry}" \
+            --expose-token  \
+            2>/dev/null \
+            | jq -r '.accessToken'
+    }
+
+
+    podman_login() {
+        local -r registries=("${@}")
+
+        for registry in "${registries[@]}"; do
+            printf "%s: %s\n" "${registry}" "$(
+                acr_password "${registry}" \
+                    | podman login \
+                        --username "${username}" \
+                        --password-stdin \
+                        "${registry}.azurecr.io"
+            )"
+        done
+    }
+
+
+    podman_login "${@}"
+    unset -f acr_password podman_login
+}
